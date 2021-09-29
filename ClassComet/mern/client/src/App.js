1c247e6e2221c14fabe5a27df2c2012b45d027b3
Cody Hanes <codyhanes@Roberts-iMac.attlocal.net>
2021-09-29 14:11:40 -0700

    simple login UI with functionality

--- ClassComet/mern/client/src/App.js
+++ ClassComet/mern/client/src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
-
 // We use Route in order to define the different routes of our application
-import { Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Login from './login.js';
+//import { Route } from "react-router-dom";
 
 // We import all the components we need in our app
 import Navbar from "./components/navbar";
@@ -11,16 +12,13 @@ import RecordList from "./components/recordList";
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
-      <Route exact path="/">
-        <RecordList />
-      </Route>
-      <Route path="/edit/:id" component={Edit} />
-      <Route path="/create">
-        <Create />
-      </Route>
-    </div>
+    <div className="app">
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Login} />
+    </Switch>
+  </Router>
+</div>
   );
 };
 
---
+++ ClassComet/mern/client/src/firebase.js
@@ -0,0 +1,88 @@
+import { initializeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics";
+import firebase from "firebase";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
+const auth = app.auth()
+const db = app.firestore();
+
+const googleProvider = new firebase.auth.GoogleAuthProvider();
+
+const signInWithGoogle = async () => {
+  try {
+    const res = await auth.signInWithPopup(googleProvider);
+    const user = res.user;
+    const query = await db
+      .collection("users")
+      .where("uid", "==", user.uid)
+      .get();
+    if (query.docs.length === 0) {
+      await db.collection("users").add({
+        uid: user.uid,
+        name: user.displayName,
+        authProvider: "google",
+        email: user.email,
+      });
+    }
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+const signInWithEmailAndPassword = async (email, password) => {
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+    } catch (err) {
+      console.error(err);
+      alert(err.message);
+    }
+  };
+
+  const registerWithEmailAndPassword = async (name, email, password) => {
+    try {
+      const res = await auth.createUserWithEmailAndPassword(email, password);
+      const user = res.user;
+      await db.collection("users").add({
+        uid: user.uid,
+        name,
+        authProvider: "local",
+        email,
+      });
+    } catch (err) {
+      console.error(err);
+      alert(err.message);
+    }
+  };
+
+  const sendPasswordResetEmail = async (email) => {
+    try {
+      await auth.sendPasswordResetEmail(email);
+      alert("Password reset link sent!");
+    } catch (err) {
+      console.error(err);
+      alert(err.message);
+    }
+  };
+
+  const logout = () => {
+    auth.signOut();
+  };
+
+  export {
+    auth,
+    db,
+    signInWithGoogle,
+    signInWithEmailAndPassword,
+    registerWithEmailAndPassword,
+    sendPasswordResetEmail,
+    logout,
+  };
