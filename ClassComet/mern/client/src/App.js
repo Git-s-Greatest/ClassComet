683157859ba7b144e3d8ea95b405d70c8103d9ef
codyhanes32 <codyhanes324@yahoo.com>
2021-10-12 10:04:36 -0700

    Added route for makequiz page

--- ClassComet/mern/client/src/App.js
+++ ClassComet/mern/client/src/App.js
@@ -5,6 +5,7 @@ import Login from './login.js';
 import Register from './register.js';
 import Landing from './landing.js';
 import LearnMore from './learnmore.js';
+import Quiz from './makequiz.js';
 //import { Route } from "react-router-dom";
 
 // We import all the components we need in our app
@@ -22,6 +23,7 @@ const App = () => {
       <Route exact path="/login" component={Login} />
       <Route exact path="/register" component={Register} />
       <Route exact path="/learnmore" component={LearnMore} />
+      <Route exact path="/makequiz" component={Quiz} />
     </Switch>
   </Router>
 </div>
