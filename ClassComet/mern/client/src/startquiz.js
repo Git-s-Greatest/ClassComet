bb7f1974e137ba250e45226b13a526279ced9a57
Jose-Roman <josejroman@cpp.edu>
2021-11-15 16:38:02 -0800

    started startquiz ui, still need to parse console

--- ClassComet/mern/client/src/startquiz.js
+++ ClassComet/mern/client/src/startquiz.js
@@ -37,6 +37,14 @@ render() {
 <div ><img src={comet} alt="Class Comet" class="logo-corner"/></div>
 
 <button type="button" value={this.props.location.state.detail} onClick={this.loadQuestion}>load</button>
+<button type="button"  class = "copy_button" onClick={() => {navigator.clipboard.writeText(this.props.location.state.detail)}}>Quiz Code: {this.props.location.state.detail}</button> 
+
+<button class = "question_view" type="button" >Question</button>
+<button class = "first_view" type="button" >1st</button>
+<button class = "second_view" type="button" >2nd</button>
+<button class = "third_view" type="button" >3rd</button>
+<button class = "fourth_view" type="button" >4th</button>
+
 
     
 </div>
